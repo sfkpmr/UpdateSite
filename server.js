fef51d00633e21258ff716620953834ca6e923d5
sfkpmr <sfkpmr@users.noreply.github.com>
2021-12-04 01:56:12 +0100

    Added API

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var express = require('express');
 const ejs = require('ejs');
 var fs = require('fs');
 const util = require('util');
+const { url } = require('inspector');
 const readFile = util.promisify(fs.readFile);
 // Initialise Express
 var app = express();
@@ -25,7 +26,7 @@ console.log("Server listening on 8080")
 
 app.get('/', async function (req, res) {
 
-    var software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
+    const software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
 
     software.sort((a, b) => {
 
@@ -50,7 +51,7 @@ app.get('/', async function (req, res) {
 
 app.get('/mobile', async function (req, res) {
 
-    var software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
+    const software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
 
     software.sort((a, b) => {
 
@@ -73,3 +74,43 @@ app.get('/mobile', async function (req, res) {
 
 });
 
+app.get('/api', (req, res) => {
+    const software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
+
+    const filteredSoftware = software.map(({ guideURL, description, box, ...keepAttrs }) => keepAttrs)
+
+    return res.send(filteredSoftware);
+});
+
+app.get('/api/:name', (req, res) => {
+    const software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
+
+    if (!software.some(e => e.name === req.params.name)) {
+        return res.send("No such software! Names are case sensitive, use spelling from https://www.wallen.co");
+    }
+
+    const querriedSoftware = software.find(e => e.name === req.params.name);
+    delete querriedSoftware.guideURL;
+    delete querriedSoftware.description;
+    delete querriedSoftware.box;
+
+    return res.send(querriedSoftware);
+});
+
+app.get('/api/:name/:version', (req, res) => {
+    const software = JSON.parse(fs.readFileSync('json/software.json', 'utf8'));
+
+    if (!software.some(e => e.name === req.params.name)) {
+        return res.send("No such software! Names are case sensitive, use spelling from https://www.wallen.co");
+    }
+
+    const querriedSoftware = software.find(e => e.name === req.params.name);
+    delete querriedSoftware.name;
+    delete querriedSoftware.releaseDate;
+    delete querriedSoftware.releaseURL;
+    delete querriedSoftware.guideURL;
+    delete querriedSoftware.description;
+    delete querriedSoftware.box;
+
+    return res.send(querriedSoftware);
+});
